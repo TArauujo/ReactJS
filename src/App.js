b1cb15631d4b457c91ed1ea14b20404c79f2075c
thomas <thomascaraujo19@gmail.com>
2023-12-22 19:10:02 -0300

    ReactDeNovo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ManageData from './components/ManageData';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Avançando em react</h1>
+      <div>
+        <img src="{boleta&vesgo}" alt="cinturinha" />
+      </div>
+      <ManageData/>
     </div>
   );
 }
